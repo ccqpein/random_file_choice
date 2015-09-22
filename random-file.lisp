bc5b78c01d7b013012d2b8846f0aba1a41a547ac
ccQpein <ccqpein@protonmail.com>
2015-09-22 14:51:12 -0400

    try to use external app, but faild

--- random-file.lisp
+++ random-file.lisp
@@ -14,3 +14,5 @@
 (defun choice_the_video (*list_of_files*)
   (print (nth (random (length *list_of_files*)) *list_of_files*)))
 
+(defun open_by_system (dir)
+  (sb-ext:run-program "/usr/bin/open" (list "-a" "Preview" dir)))
