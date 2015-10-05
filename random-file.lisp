f542fe768206576183c9a6bcec87954615808568
ccQpein <ccqpein@protonmail.com>
2015-10-05 18:49:27 -0400

    change sth

--- random-file.lisp
+++ random-file.lisp
@@ -3,17 +3,19 @@
 (ql:quickload 'inferior-shell)
 
 (defvar *list-of-files* nil)
+(defvar *list-all-files* nil)
 
 (defun get-files-or-dir (dir-input)
-  (cl-fad:list-directory dir-input))
+  (cl-fad:walk-directory dir-input #'(lambda (x) (push x *list-all-files*))))
 
 (defun push-to-list (dir-input)
   (push (get-files-or-dir dir-input) *list-of-files*))
 
 (defun push-to-list2 (dir-input)
-  (dolist (x (get-files-or-dir dir-input))
-    (cond ((not (cl-fad:directory-pathname-p x))
-    (push x *list-of-files*)))))
+  (get-files-or-dir dir-input)
+  (dolist (x *list-all-files*)
+    (if (find (pathname-type x) '("db" "lisp"))
+	(push x *list-of-files*))))
 
 (defun open-by-system (file)
   (inferior-shell:run/ss (list "open" file)))
