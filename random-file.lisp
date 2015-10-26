9a05d0187d8699b5e6fb4f5189e230c61d544c02
ccQpein <ccqpein@protonmail.com>
2015-10-26 15:26:59 -0400

    not need define var any more

--- random-file.lisp
+++ random-file.lisp
@@ -2,7 +2,7 @@
 (load "/Users/ccQ/quicklisp/setup.lisp")
 (ql:quickload 'inferior-shell)
 
-(defvar *list-of-files* nil)
+;(defvar *list-of-files* nil)
 
 (defun get-files-or-dir (dir-input)
   (let (files)
@@ -11,21 +11,25 @@
     files))
 
 (defun push-to-list (dir-input)
-  (push (get-files-or-dir dir-input) *list-of-files*))
+  (let (listoffiles)
+    (dolist (x (get-files-or-dir dir-input))
+      (loop for i in '("avi" "rmvb" "MOV")
+	 when (equalp (pathname-type x) i)
+	   do (push x listoffiles)))
+    listoffiles))
 
-(defun push-to-list2 (dir-input)
-  (dolist (x (get-files-or-dir dir-input))
-    (loop for i in '("avi" "rmvb")
-       when (equalp (pathname-type x) i)
-	 do (push x *list-of-files*))))
+;(defun push-to-list2 (dir-input)
+;  (dolist (x (get-files-or-dir dir-input))
+;    (loop for i in '("avi" "rmvb")
+;       when (equalp (pathname-type x) i)
+;	 do (push x *list-of-files*))))
 
 (defun open-by-system (file)
   (inferior-shell:run/ss (list "open" file)))
 
-(defun choice-file-to-open ()
-  (open-by-system (nth (random (length *list-of-files*)) *list-of-files*)))
+(defun choice-file-to-open (files)
+  (open-by-system (nth (random (length files)) files)))
 
 (defun main ()
-  (push-to-list2 (read))
-  (choice-file-to-open )
+  (choice-file-to-open (push-to-list2 (read)))
   )
